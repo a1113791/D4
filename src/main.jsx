32e4ffdb4ef626a53e9f7d6f70b7d861752972d5
hua <a1113791@gmail.com>
2025-02-18 00:15:12 +0800

    bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-//import "./assets/scss/all.scss";
-import "bootstrap/dist/css/bootstrap.min.css"; // 引入 CSS
+import "../stylesheets/all.scss";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
